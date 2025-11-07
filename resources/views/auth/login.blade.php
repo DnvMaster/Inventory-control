daa9158025c5304e2ab19ebcbd3bb4372edc8193
DnvMaster <dnvcomp@yandex.ru>
2025-11-07 19:39:22 +0300

    Account login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,87 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>DnvMaster | Вход в аккаунт</title>
+        <link rel="shortcut icon" href="{{ asset('backend/images/favicon.ico') }}">
+        <link href="{{ asset('backend/css/app.min.css') }}" rel="stylesheet" id="app-style">
+        <link href="{{ asset('backend/css/icons.min.css') }}" rel="stylesheet">
+    </head>
+    <body class="bg-white">
+        <div class="account-page">
+            <div class="container-fluid p-0">
+                <div class="row align-items-center g-0">
+                    <div class="col-xl-5">
+                        <div class="row">
+                            <div class="col-md-7 mx-auto">
+                                <div class="mb-0 border-0 p-md-5 p-lg-0 p-4">
+                                    <div class="mb-4 p-0">
+                                        <a href="index.html" class="auth-logo">
+                                            <img src="{{ asset('backend/images/logo-dark.png') }}" alt="logo-dark" class="mx-auto" height="28" />
+                                        </a>
+                                    </div>
+                                    <div class="pt-0">
+                                        <form action="{{ route('login') }}" method="post" class="my-4">
+                                            @csrf
+                                            <div class="form-group mb-3">
+                                                <label for="emailaddress" class="form-label">Електронная почта</label>
+                                                <input class="form-control" type="email" name="email" id="emailaddress" required="" placeholder="Введите свой Е-мэйл">
+                                            </div>
+                
+                                            <div class="form-group mb-3">
+                                                <label for="password" class="form-label">Пароль</label>
+                                                <input class="form-control" type="password" name="password" required="" id="password" placeholder="Введите свой пароль">
+                                            </div>
+                
+                                            <div class="form-group d-flex mb-3">
+                                                <div class="col-sm-6">
+                                                    <div class="form-check">
+                                                        <input type="checkbox" class="form-check-input" id="checkbox-signin" checked>
+                                                        <label class="form-check-label" for="checkbox-signin">Запомнить</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-6 text-end">
+                                                    @if (Route::has('password.request'))
+                                                        <a class='text-muted fs-14' href='{{ route('password.request') }}'>Восстановить пароль?</a>
+                                                    @endif                             
+                                                </div>
+                                            </div>
+                                            <div class="form-group mb-0 row">
+                                                <div class="col-12">
+                                                    <div class="d-grid">
+                                                        <button class="btn btn-primary" type="submit"> Войти </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </form>
+                                        <div class="saprator my-4"><span>или</span></div>
+                                        <div class="text-center text-muted mb-4">
+                                            <p class="mb-0">У вас ещё нет аккаунта ?<a class='text-primary ms-2 fw-medium' href='{{ route('register') }}'>Зарегистрируйтесь</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-7">
+                        <div class="account-page-bg p-md-5 p-4">
+                            <div class="text-center">
+                                <div class="auth-image">
+                                    <img src="{{ asset('backend/images/authentication.svg') }}" class="mx-auto img-fluid"  alt="images">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+        <script src="{{ asset('backend/libs/jquery/jquery.min.js') }}"></script>
+        <script src="/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="/libs/simplebar/simplebar.min.js') }}"></script>
+        <script src="{{ asset('backend/libs/node-waves/waves.min.js') }}"></script>
+        <script src="{{ asset('backend/libs/waypoints/lib/jquery.waypoints.min.js') }}"></script>
+        <script src="{{ asset('backend/libs/jquery.counterup/jquery.counterup.min.js') }}"></script>
+        <script src="{{ asset('backend/libs/feather-icons/feather.min.js') }}"></script>
+        <script src="{{ asset('backend/js/app.js') }}"></script>
+    </body>
+</html>
