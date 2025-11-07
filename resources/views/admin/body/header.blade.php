1896bd060bd92fa7e1e2ea63c869b0759f42f64f
DnvMaster <dnvcomp@yandex.ru>
2025-11-07 16:13:42 +0300

    Install admin template

---
+++ resources/views/admin/body/header.blade.php
@@ -0,0 +1,67 @@
+<div class="topbar-custom">
+                <div class="container-xxl">
+                    <div class="d-flex justify-content-between">
+                        <ul class="list-unstyled topnav-menu mb-0 d-flex align-items-center">
+                            <li>
+                                <button class="button-toggle-menu nav-link ps-0">
+                                    <i data-feather="menu" class="noti-icon"></i>
+                                </button>
+                            </li>
+                            <li class="d-none d-lg-block">
+                                <div class="position-relative topbar-search">
+                                    <input type="text" class="form-control bg-light bg-opacity-75 border-light ps-4" placeholder="Search...">
+                                    <i class="mdi mdi-magnify fs-16 position-absolute text-muted top-50 translate-middle-y ms-2"></i>
+                                </div>
+                            </li>
+                        </ul>
+
+                        <ul class="list-unstyled topnav-menu mb-0 d-flex align-items-center">
+
+                            <li class="d-none d-sm-flex">
+                                <button type="button" class="btn nav-link" data-toggle="fullscreen">
+                                    <i data-feather="maximize" class="align-middle fullscreen noti-icon"></i>
+                                </button>
+                            </li>
+        
+                            <li class="dropdown notification-list topbar-dropdown">
+                                <a class="nav-link dropdown-toggle nav-user me-0" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                                    <img src="{{ asset('backend/images/users/dnvmaster.jpg') }}" alt="user-image" class="rounded-circle">
+                                    <span class="pro-user-name ms-1">
+                                        Николай <i class="mdi mdi-chevron-down"></i> 
+                                    </span>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-end profile-dropdown ">
+                                    <!-- item-->
+                                    <div class="dropdown-header noti-title">
+                                        <h6 class="text-overflow m-0">Привет !</h6>
+                                    </div>
+        
+                                    <!-- item-->
+                                    <a href="pages-profile.html" class="dropdown-item notify-item">
+                                        <i class="mdi mdi-account-circle-outline fs-16 align-middle"></i>
+                                        <span>Аккаунт</span>
+                                    </a>
+        
+                                    <!-- item-->
+                                    <a href="auth-lock-screen.html" class="dropdown-item notify-item">
+                                        <i class="mdi mdi-lock-outline fs-16 align-middle"></i>
+                                        <span>Блокировка</span>
+                                    </a>
+        
+                                    <div class="dropdown-divider"></div>
+        
+                                    <!-- item-->
+                                    <a href="auth-logout.html" class="dropdown-item notify-item">
+                                        <i class="mdi mdi-location-exit fs-16 align-middle"></i>
+                                        <span>Выход</span>
+                                    </a>
+        
+                                </div>
+                            </li>
+        
+                        </ul>
+                    </div>
+
+                </div>
+               
+            </div>
